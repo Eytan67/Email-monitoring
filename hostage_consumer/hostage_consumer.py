b08f4127e03da0ba8cf238fe23af06be24dbc41b
eytan zichel <eytanzichel@gmail.com>
2024-11-14 17:05:23 +0200

    consumer for hostage emails, consume on messages.hostage topik get the emails and store them in postgres

---
+++ hostage_consumer/hostage_consumer.py
@@ -0,0 +1,42 @@
+import json
+import psycopg2
+from kafka import KafkaConsumer
+from explosive_comsumer.database import db_session
+from explosive_comsumer.models import create_email_obj, \
+    create_location_obj, create_device_info_obj, create_suspicious_hostage_content_obj
+
+consumer = KafkaConsumer(
+    'messages.hostage',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='hostage_messages_group',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+)
+
+
+for email in consumer:
+    email_data = email.value
+    print(f"received message: {email_data['email']}")
+
+    try:
+        email_obj = create_email_obj(email_data)
+        email_id = email_obj.id
+
+        location_obj = create_location_obj(email_data['location'], email_id)
+
+        device_info_obj = create_device_info_obj(email_data['device_info'], email_id)
+
+        sentences_data = ' '.join(email_data['sentences'])
+        suspicious_content_obj = create_suspicious_hostage_content_obj(sentences_data, email_id)
+
+
+        db_session.add(email_obj)
+        db_session.add(location_obj)
+        db_session.add(device_info_obj)
+        db_session.add(suspicious_content_obj)
+        db_session.commit()
+
+    except Exception as e:
+        db_session.rollback()
+        print(f"Error processing message: {e}")
