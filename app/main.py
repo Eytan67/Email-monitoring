97462ef52d2431c686695a7fecc34a7d9469b754
eytan zichel <eytanzichel@gmail.com>
2024-11-14 11:36:55 +0200

    adding the producer part to the main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from flask import Flask, jsonify, request, abort
+from kafka import KafkaProducer
+import json
 
 
 app = Flask(__name__)
 
+producer = KafkaProducer(
+        bootstrap_servers='localhost:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
 @app.route('/api/email', methods=['POST'])
 def get_emails():
     data = request.json
     if not data:
         abort(400, description="Missing data")
+    producer.send('messages.all', data)
+    print(data)
     return jsonify(), 200
 
 if __name__ == '__main__':
