6538608f53a9a4cf81238bed6342c8d10b65a653
eytan zichel <eytanzichel@gmail.com>
2024-11-14 11:33:13 +0200

    consumer of a stream processor calling the route email function to detect and route the emails for the next stap storing the email in the mongo

---
+++ stream_processor/al_messages_consumer.py
@@ -0,0 +1,22 @@
+import json
+from kafka import KafkaConsumer
+
+from producer import route_message
+from mongodb_connection import get_mongo_connection
+
+
+consumer = KafkaConsumer(
+    'messages.all',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='all_messages_group',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+)
+
+for email in consumer:
+    email = email.value
+    route_email(email)
+    collection = get_mongo_connection()
+    collection.insert_one(email)
+
